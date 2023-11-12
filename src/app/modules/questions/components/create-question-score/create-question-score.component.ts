976fbfb2116246fc772820a12ca3af5602bfb364
mechztcc <dojathan@gmail.com>
2023-11-12 09:41:37 -0500

    feat: improviments on create question score

--- src/app/modules/questions/components/create-question-score/create-question-score.component.ts
+++ src/app/modules/questions/components/create-question-score/create-question-score.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faBook } from '@fortawesome/free-solid-svg-icons';
+import { faBolt, faBook } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-create-question-score',
@@ -9,11 +9,18 @@ import { faBook } from '@fortawesome/free-solid-svg-icons';
 export class CreateQuestionScoreComponent {
   icons = {
     book: faBook,
+    score: faBolt,
   };
+
+  score: number;
 
   scores = [
     { name: '1', id: 1 },
     { name: '2', id: 2 },
     { name: '3', id: 3 },
   ];
+
+  onSelect(value: number) {
+    this.score = value;
+  }
 }
